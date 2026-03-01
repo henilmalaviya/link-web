700607187bc176c4bd51e7307b80a5e4f4aea539
Henil Malaviya <henilm.dev@gmail.com>
2026-03-01 14:41:43 +0530

    chore: fix seach icon alignment

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -73,7 +73,7 @@
 
 		<div class="flex items-center gap-2">
 			<div class="relative grow">
-				<Search class="absolute top-2.5 left-2.5 h-4 w-4 text-muted-foreground" />
+				<Search class="absolute top-1/2 left-2.5 h-4 w-4 -translate-y-1/2 text-muted-foreground" />
 				<Input
 					type="text"
 					placeholder="Search by short link or URL"
